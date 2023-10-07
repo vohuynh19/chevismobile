cf07cdf1d05470c28451333a4a1cc2564f699d5b
VoHuynh <huynh.vo@unifiedpost.com>
2023-10-07 13:53:32 +0700

    feat: refactor img

--- src/bottom-sheets/naive/PrescriptionHistoryBottomSheet.tsx
+++ src/bottom-sheets/naive/PrescriptionHistoryBottomSheet.tsx
@@ -1,9 +1,7 @@
 import {Modalize} from 'react-native-modalize';
 
-import {images} from '~assets';
-import {colors} from '~core/theme';
 import {screenHeight} from '~core/utils';
-import {FastImage, Text, View} from '~core/ui';
+import {Text, View} from '~core/ui';
 
 export const PrescriptionHistoryBottomSheet = () => {
   return (
@@ -12,13 +10,6 @@ export const PrescriptionHistoryBottomSheet = () => {
       alwaysOpen={(screenHeight * 2) / 5}
       avoidKeyboardLikeIOS={true}>
       <View alignItems="center" mt={8}>
-        <FastImage
-          tintColor={colors.neutral800}
-          source={images.tumbleE}
-          resizeMode="contain"
-          style={{width: 160, height: 160}}
-        />
-
         <Text mt={6}>Let's do your first eye checking!</Text>
       </View>
     </Modalize>
